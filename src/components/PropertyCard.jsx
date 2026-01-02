0312e33b57d1a264b60f683def2ad709c6223b34
dasu2003 <dasunthasamarasinghe@gmail.com>
2026-01-02 13:29:11 +0530

    Update PropertyCard.jsx

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -15,7 +15,7 @@ function PropertyCard({ property, onView, onFavorite, isFavorite, onDragStart })
     >
       {/* Property Image */}
       <img
-        src={property.images[0]}
+        src={process.env.PUBLIC_URL + '/' + property.images[0]}
         alt={property.location}
         className="property-image"
         onError={handleImageError}
