239a59205733aac364b003b1b788f78febabb1bf
dasu2003 <dasunthasamarasinghe@gmail.com>
2026-01-03 02:13:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter basename="/estate-agent">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
